8652138788ae637f7590adbdef003d81c9740e88
Peter <bozhko999@mail.ru>
2020-10-04 17:02:50 +0300

    add colors, styles, done activity

--- app/src/main/java/com/itmo/wineup/features/catalog/presentation/filters/FilterColorActivity.kt
+++ app/src/main/java/com/itmo/wineup/features/catalog/presentation/filters/FilterColorActivity.kt
@@ -1,65 +1,49 @@
 package com.itmo.wineup.features.catalog.presentation.filters
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.CheckBox
-import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import com.itmo.wineup.R
 import com.itmo.wineup.features.catalog.models.WineColor
+import kotlinx.android.synthetic.main.activity_filter_color.*
+
 
 class FilterColorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_color)
     }
-
-
     override fun finish() {
         super.finish()
         overridePendingTransition(0, R.anim.noanimation)
     }
-    public val colors = mutableListOf<WineColor>(WineColor.RED, WineColor.PINK, WineColor.WHITE)
+    private val colors = mutableListOf<WineColor>(WineColor.RED)
     fun onCheckboxClicked(view: View) {
-        if (view is CheckBox) {
-            val checked: Boolean = view.isChecked
-            when (view.id) {
-                R.id.whiteWineCheckBox -> {
-                    if (checked) {
-                        if (WineColor.WHITE !in colors) {
-                            colors.add(WineColor.WHITE)
-                        }
-                    } else {
-                        if (WineColor.WHITE in colors) {
-                            colors.remove(WineColor.WHITE)
-                        }
-                    }
-                }
-                R.id.redWineCheckBox -> {
-                    if (checked) {
-                        if (WineColor.RED !in colors) {
-                            colors.add(WineColor.RED)
-                        }
-                    } else {
-                        if (WineColor.RED in colors) {
-                            colors.remove(WineColor.RED)
-                        }
-                    }
-                }
-                R.id.pinkWineCheckBox -> {
-                    if (checked) {
-                        if (WineColor.PINK !in colors) {
-                            colors.add(WineColor.PINK)
-                        }
-                    } else {
-                        if (WineColor.PINK in colors) {
-                            colors.remove(WineColor.PINK)
-                        }
-                    }
-                }
-            }
-        }
+
+        if (whiteWineCheckBox.isChecked) {
+            if (WineColor.WHITE !in colors) colors.add(WineColor.WHITE)
+        } else colors.remove(WineColor.WHITE)
+
+        if (redWineCheckBox.isChecked) {
+            if (WineColor.RED !in colors) colors.add(WineColor.RED)
+        } else colors.remove(WineColor.RED)
+
+        if (pinkWineCheckBox.isChecked) {
+            if (WineColor.PINK !in colors) colors.add(WineColor.PINK)
+        } else colors.remove(WineColor.PINK)
 
     }
+    fun onResetClick(view: View) {
+        whiteWineCheckBox.isChecked = false
+        redWineCheckBox.isChecked = false
+        pinkWineCheckBox.isChecked = false
+        colors.clear()
+    }
+
+    fun onConfirmClick(view: View) {
+//        TODO return result (colors)
+        finish()
+    }
+
 }
