685168db5d1a7c6f241d74d3c2efb73acb48aebb
hotline4beach <zrbtmn@gmail.com>
2020-10-12 12:08:13 +0300

    Added age checking pages

--- app/src/main/java/com/itmo/wineup/features/auth/AgeAccessActivity.kt
+++ app/src/main/java/com/itmo/wineup/features/auth/AgeAccessActivity.kt
@@ -1,12 +1,23 @@
 package com.itmo.wineup.features.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itmo.wineup.MainActivity
 import com.itmo.wineup.R
+import kotlinx.android.synthetic.main.activity_age_access.*
 
 class AgeAccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_age_access)
+        button2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        button3.setOnClickListener{
+            val intent = Intent(this, AgeAccessErrorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/itmo/wineup/features/auth/AgeAccessErrorActivity.kt
+++ app/src/main/java/com/itmo/wineup/features/auth/AgeAccessErrorActivity.kt
@@ -1,12 +1,18 @@
 package com.itmo.wineup.features.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.itmo.wineup.R
+import kotlinx.android.synthetic.main.activity_age_access_error.*
 
 class AgeAccessErrorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_age_access_error)
+        button.setOnClickListener{
+            val intent = Intent(this, AgeAccessActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
