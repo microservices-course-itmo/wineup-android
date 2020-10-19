28747b00bcfe5c738acb64de197c31aa8013109b
Vyacheslav <chearilando@gmail.com>
2020-10-19 18:53:07 +0300

    Created SortFragment

--- app/src/main/java/com/itmo/wineup/features/favorites/presentation/SortFragment.kt
+++ app/src/main/java/com/itmo/wineup/features/favorites/presentation/SortFragment.kt
@@ -1,22 +1,26 @@
 package com.itmo.wineup.features.favorites.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.itmo.wineup.R
+import com.itmo.wineup.features.favorites.presentation.models.FavoriteFilterModel
 import com.itmo.wineup.features.catalog.presentation.CatalogViewModel
-import kotlinx.android.synthetic.main.fragment_filter_color.*
+import kotlinx.android.synthetic.main.fragment_sort.*
 
 
 class SortFragment: BottomSheetDialogFragment() {
 
+    private lateinit var viewModel: CatalogViewModel
+
+    private val sort = mutableSetOf(FavoriteFilterModel.BY_RECOMMENDATION)
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_sort, container, false)
@@ -25,6 +29,46 @@ class SortFragment: BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(CatalogViewModel::class.java)
+
+
+    }
+
+    private fun setFilter(sorting: Set<FavoriteFilterModel>){
+        for(sort in sorting)
+            when(sort){
+                FavoriteFilterModel.BY_RECOMMENDATION-> byRecommendCheckbox.isChecked = true
+                FavoriteFilterModel.BY_RATING -> byRatingCheckbox.isChecked = true
+                FavoriteFilterModel.BY_PRICE_MIN -> ascendingPriceCheckbox.isChecked = true
+                FavoriteFilterModel.BY_PRICE_MAX -> descendingPriceCheckbox.isChecked = true
+            }
+    }
+
+    private fun onCheckboxClicked() {
+        if (byRecommendCheckbox.isChecked) {
+            sort.add(FavoriteFilterModel.BY_RECOMMENDATION)
+        } else {
+            sort.remove(FavoriteFilterModel.BY_RECOMMENDATION)
+        }
+
+        if (byRatingCheckbox.isChecked) {
+            sort.add(FavoriteFilterModel.BY_RATING)
+        } else {
+            sort.remove(FavoriteFilterModel.BY_RATING)
+        }
+
+        if (ascendingPriceCheckbox.isChecked) {
+            sort.add(FavoriteFilterModel.BY_PRICE_MIN)
+        } else {
+            sort.remove(FavoriteFilterModel.BY_PRICE_MIN)
+        }
+
+        if (descendingPriceCheckbox.isChecked) {
+            sort.add(FavoriteFilterModel.BY_PRICE_MAX)
+        } else {
+            sort.remove(FavoriteFilterModel.BY_PRICE_MAX)
+        }
 
     }
 }
+
