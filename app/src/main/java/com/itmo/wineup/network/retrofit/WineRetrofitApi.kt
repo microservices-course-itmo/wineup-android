e7f8fe20669bd66854f214ba32342688d01641ad
rybakovi <rybakoff888ilia@gmail.com>
2020-11-25 17:02:52 +0300

    add method for position

--- app/src/main/java/com/itmo/wineup/network/retrofit/WineRetrofitApi.kt
+++ app/src/main/java/com/itmo/wineup/network/retrofit/WineRetrofitApi.kt
@@ -1,5 +1,6 @@
 package com.itmo.wineup.network.retrofit
 
+import com.itmo.wineup.network.retrofit.data.WinePositionResponse
 import com.itmo.wineup.network.retrofit.data.WineResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -9,5 +10,9 @@ interface WineRetrofitApi {
     @Headers("accessToken: 123")
     @GET("wine/true")
     suspend fun getWineList(): List<WineResponse>
+
+    @Headers("accessToken: 123")
+    @GET("position/true")
+    suspend fun getWinePositionList(): List<WinePositionResponse>
 
 }
