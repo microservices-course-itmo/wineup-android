1cd8d3e84b11e5e3d98c53cbaff1989226940150
Peter <bozhko999@mail.ru>
2021-02-23 13:05:29 +0300

    add logout alert

--- app/src/main/java/com/itmo/wineup/features/profile/presentation/ProfileFragment.kt
+++ app/src/main/java/com/itmo/wineup/features/profile/presentation/ProfileFragment.kt
@@ -1,17 +1,18 @@
 package com.itmo.wineup.features.profile.presentation
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.itmo.wineup.R
 import com.itmo.wineup.features.auth.USER_ACCESS_INFO
-import com.itmo.wineup.features.auth.USER_CURRENT_ID
 import com.itmo.wineup.features.auth.presentation.AgeAccessActivity
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -27,7 +28,8 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?): View = inflater.inflate(R.layout.fragment_profile, container, false)
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_profile, container, false)
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,11 +39,42 @@ class ProfileFragment : Fragment() {
         viewModel.nameLiveData.observe(viewLifecycleOwner, Observer(profile_name::setText))
         viewModel.currentUser()
         profile_exit_button.setOnClickListener {
-            val preferences = requireActivity().getSharedPreferences(USER_ACCESS_INFO, Context.MODE_PRIVATE)
-            preferences.edit().clear().apply()
-            val exitIntent = Intent(requireContext().applicationContext, AgeAccessActivity::class.java)
-            exitIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(exitIntent)
+            showLogOutAlert()
+        }
+    }
+
+    private fun logOut() {
+        val preferences =
+            requireActivity().getSharedPreferences(USER_ACCESS_INFO, Context.MODE_PRIVATE)
+        preferences.edit().clear().apply()
+        val exitIntent = Intent(requireContext().applicationContext, AgeAccessActivity::class.java)
+        exitIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(exitIntent)
+    }
+
+    private fun showLogOutAlert() {
+        val dialog = AlertDialog.Builder(requireContext())
+            .setMessage(requireContext().getString(R.string.log_out_alert))
+            .setCancelable(false)
+            .setPositiveButton(
+                requireContext().getString(R.string.global_yes),
+                { dialogInterface: DialogInterface, i: Int ->
+                    dialogInterface.cancel()
+                    logOut()
+                })
+            .setNegativeButton(
+                requireContext().getString(R.string.global_no),
+                { dialogInterface: DialogInterface, i: Int ->
+                    dialogInterface.cancel()
+                })
+            .show()
+        with(dialog.getButton(AlertDialog.BUTTON_POSITIVE)) {
+            setTextColor(context.getColor(R.color.red))
+            setTypeface(android.graphics.Typeface.DEFAULT, android.graphics.Typeface.BOLD)
+        }
+        with(dialog.getButton(AlertDialog.BUTTON_NEGATIVE)) {
+            setTextColor(context.getColor(R.color.red))
+            setTypeface(android.graphics.Typeface.DEFAULT, android.graphics.Typeface.BOLD)
         }
     }
 
