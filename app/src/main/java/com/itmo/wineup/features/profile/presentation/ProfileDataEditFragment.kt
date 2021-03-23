d3c23ccf8a8b749956020baef9aedb84b99d4068
rybakovi <rybakoff888ilia@gmail.com>
2021-03-23 13:02:47 +0300

    add navigation to edit screen

---
+++ app/src/main/java/com/itmo/wineup/features/profile/presentation/ProfileDataEditFragment.kt
@@ -0,0 +1,82 @@
+package com.itmo.wineup.features.profile.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.itmo.wineup.R
+import com.itmo.wineup.features.profile.model.Profile
+import kotlinx.android.synthetic.main.profile_data_change_fragment.*
+
+class ProfileDataEditFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.profile_data_change_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val profileData = arguments?.getSerializable("profile") as Profile
+        with (profileData){
+            userName.setText(name)
+            userPhone.setText(phone)
+        }
+    }
+
+}
+
+/*
+    //todo: to edit info screen
+    private fun showChangePhoneAlert() {
+        val dialog = AlertDialog.Builder(requireContext())
+            .setMessage(requireContext().getString(R.string.linking_phone_number))
+            .setCancelable(false)
+            .setPositiveButton(requireContext().getString(R.string.global_yes)) { dialogInterface: DialogInterface, i: Int -> dialogInterface.cancel()
+                (activity as MainActivity).openConfirmCodefragment(profile_phone.text.toString())
+                phoneChangeAvailability()
+            }
+            .setNegativeButton(requireContext().getString(R.string.global_no)) { dialogInterface: DialogInterface, i: Int -> dialogInterface.cancel()
+            }
+            .show()
+        with(dialog.getButton(AlertDialog.BUTTON_POSITIVE)) {
+            setTextColor(context.getColor(R.color.red))
+            setTypeface(android.graphics.Typeface.DEFAULT, android.graphics.Typeface.BOLD)
+        }
+        with(dialog.getButton(AlertDialog.BUTTON_NEGATIVE)) {
+            setTextColor(context.getColor(R.color.red))
+            setTypeface(android.graphics.Typeface.DEFAULT, android.graphics.Typeface.BOLD)
+        }
+    }*/
+
+/* код кастомных алертов Славы, пока пусть закомментирован пусть будет
+private fun showChangePhoneAlert() {
+       val mDialogView = LayoutInflater.from(this).inflate((R.layout.linking_phone_number, null));
+
+       val mBuilder = AlertDialog.Builder(this)
+           .setView(mDialogView)
+
+       val mAlertDialog =  mBuilder.show()
+
+       mAlertDialog.buttonYes.setOnClickListener {
+           showAccessChangePhoneAlert()
+           mAlertDialog.dismiss()
+       }
+
+       mDialogView.buttonNo.setOnClickListener {
+           mAlertDialog.dismiss()
+       }
+
+   }
+
+   private fun showAccessChangePhoneAlert() {
+       val mDialogView = LayoutInflater.from(this).inflate((R.layout.linking_phone_success, null));
+
+       val mBuilder = AlertDialog.Builder(this)
+           .setView(mDialogView)
+
+       val mAlertDialog = mBuilder.show()
+       mAlertDialog.dismiss()
+   }*/
