832b559ad3b097c5ebde0144482c421d14284aa5
Alchemic <vitalij0202@gmail.com>
2021-03-30 19:44:49 +0300

    Added error activities

--- app/src/main/java/com/itmo/wineup/features/main/presentation/MainFragment.kt
+++ app/src/main/java/com/itmo/wineup/features/main/presentation/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.itmo.wineup.features.main.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.itmo.wineup.R
+import com.itmo.wineup.features.errors.Error404Activity
+import com.itmo.wineup.features.errors.Error503Activity
+import com.itmo.wineup.features.errors.ErrorPageNotFoundActivity
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -28,6 +33,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        button1.setOnClickListener {
+            startActivity(Intent(context, Error404Activity::class.java))
+        }
+        button2.setOnClickListener {
+            startActivity(Intent(context, Error503Activity::class.java))
+        }
+        button3.setOnClickListener {
+            startActivity(Intent(context, ErrorPageNotFoundActivity::class.java))
+        }
     }
 
 }
